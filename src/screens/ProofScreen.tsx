a8f4252d18234dae80e1310636d249e8892ba427
Alexandru Pele <pelealexandru@gmil.com>
2023-05-02 12:29:07 +0300

    Simplify proof screen

--- src/screens/ProofScreen.tsx
+++ src/screens/ProofScreen.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text} from 'react-native';
 import {useRoute} from '@react-navigation/native';
 
 const ProofScreen = () => {
   const route = useRoute<any>();
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Proof data: ${route.params.proofData}</Text>
-    </View>
-  );
+  return <Text>Proof data: ${route.params.proofData}</Text>;
 };
 
 export default ProofScreen;
